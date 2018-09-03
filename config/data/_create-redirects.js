ed42e6dac74f9cabd726cd397266108729f68bb4
Peter Goes <petergoes@gmail.com>
2018-09-03 15:47:21 +0200

    refactor(_create-redirects): improve readability

--- config/data/_create-redirects.js
+++ config/data/_create-redirects.js
@@ -1,16 +1,12 @@
 const fs = require('fs')
 const path = require('path')
-const pipe = require('lodash/fp/pipe')
-const get = require('lodash/fp/get')
-const map = require('lodash/fp/map')
-const tap = require('lodash/fp/tap')
-const curry = require('lodash/fp/curry')
-const join = require('lodash/fp/join')
+const { pipe, get, map, curry, join } = require('lodash/fp')
 
 const staticDir = path.join(__dirname, '../../src/client/static')
 const sourceFile = path.join(staticDir, 'data/redirects.json')
 const targetFile = path.join(staticDir, '_redirects')
 
+const redirectToLine = redirect => `${redirect.from} ${redirect.to} ${redirect.statusCode}`
 const readFile = filePath => fs.readFileSync(filePath, { encoding: 'utf8' })
 const writeFile = curry((filePath, contents) => fs.writeFileSync(filePath, contents, { encoding: 'utf8' }))
 
@@ -18,7 +14,7 @@ pipe(
   readFile,
   JSON.parse,
   get(['redirects']),
-  map(redirect => `${redirect.from} ${redirect.to} ${redirect.statusCode}`),
+  map(redirectToLine),
   join('\n'),
   writeFile(targetFile)
 )(sourceFile)
