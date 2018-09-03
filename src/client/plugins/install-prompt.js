16c251dd07afd0df20b37b2d55cb3e5e0b92df58
Peter Goes <petergoes@gmail.com>
2018-09-03 12:02:48 +0200

    refactor: remove unneeded libs

--- src/client/plugins/install-prompt.js
+++ src/client/plugins/install-prompt.js
@@ -1,9 +1,5 @@
 window.addEventListener('beforeinstallprompt', (event) => {
   // Read this to get a better understanding of install prompts:
   // https://developers.google.com/web/fundamentals/app-install-banners/
-  ga('send', 'event', 'install', 'available')
   event.preventDefault()
-})
-window.addEventListener('appinstalled', () => {
-  ga('send', 'event', 'install', 'installed')
 })
