a358587f17fd34e0360266fc24ebc051b5fcc81a
bruna <brunadafonseca@gmail.com>
2018-09-06 14:58:05 +0200

    Display area on settings

--- src/client/pages/settings.vue
+++ src/client/pages/settings.vue
@@ -1,10 +1,11 @@
 <template>
-  <div class="project-settings">
-    <header>
+  <div class="settings">
+    <header class="settings__header">
       <h2>Settings</h2>
       <nav>
         <ul>
           <li><nuxt-link to="/en/settings/project-area">Project area</nuxt-link></li>
+          <li><nuxt-link to="/en/settings/general">General</nuxt-link></li>
         </ul>
       </nav>
     </header>
@@ -14,14 +15,24 @@
 </template>
 
 <script>
+import { mapState } from 'vuex'
+
 export default {
-  
+  computed: {
+    ...mapState({
+      projectSettings: state => state.project.settings,
+    }),
+    projectArea() { return this.projectSettings.projectArea.properties.area.toFixed(2) },
+  },
 }
 </script>
 
 <style>
-.project-settings {
+.settings {
   width: 700px;
 }
 
+.settings__header {
+  height: 100px;
+}
 </style>
--- src/client/pages/settings/project-area.vue
+++ src/client/pages/settings/project-area.vue
@@ -1,10 +1,28 @@
 <template>
-  <h2>Project area</h2>  
+  <div>
+    <h2>{{ $t('project_area') }}</h2>
+    <div v-if="area">{{ area }}m2</div>
+    <form @submit="onSubmit">
+      <input type="text">
+    </form>
+  </div>   
 </template>
 
 <script>
+import { mapState } from 'vuex'
+
 export default {
-  
+  computed: {
+    ...mapState({
+      projectArea: state => state.project.settings.projectArea,
+    }),
+    area() { return this.projectArea.properties.area.toFixed(2) },
+  },
+  methods: {
+    onSubmit() {
+      // TODO handle submit
+    },
+  },
 }
 </script>
 
