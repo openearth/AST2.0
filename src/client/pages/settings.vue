d148189e755ea6f810f9d9aaa169d542e0296786
bruna <brunadafonseca@gmail.com>
2018-09-06 11:03:26 +0200

    Add header and navigation

--- src/client/pages/settings.vue
+++ src/client/pages/settings.vue
@@ -1,6 +1,14 @@
 <template>
-  <div>
-    <h2>Settings</h2>
+  <div class="project-settings">
+    <header>
+      <h2>Settings</h2>
+      <nav>
+        <ul>
+          <li><nuxt-link to="/en/settings/project-area">Project area</nuxt-link></li>
+        </ul>
+      </nav>
+    </header>
+
     <nuxt-child />
   </div>
 </template>
@@ -12,5 +20,8 @@ export default {
 </script>
 
 <style>
+.project-settings {
+  width: 700px;
+}
 
 </style>
