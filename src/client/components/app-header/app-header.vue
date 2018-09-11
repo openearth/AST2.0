8ba6825259d853a2b7b945185086a9e3a225395b
bruna <brunadafonseca@gmail.com>
2018-09-11 11:27:52 +0200

    Add link to my-measures page on header

--- src/client/components/app-header/app-header.vue
+++ src/client/components/app-header/app-header.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="app-header">
     <h2>AST 2.0</h2>
+    <nuxt-link :to="`/${this.$i18n.locale}/my-measures`" class="app-header__link">My measures</nuxt-link>
   </div>
 </template>
 
@@ -9,7 +10,13 @@
   background-color: #2196F3;
   color: white;
   display: flex;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
+  padding: 0 var(--spacing-default);
+}
+
+.app-header__link {
+  text-decoration: none;
+  color: var(--background-color);
 }
 </style>
