51d1b3ea5fb866e7c7b74b2f8be909cb265bfedf
Peter Goes <petergoes@gmail.com>
2018-09-17 15:08:39 +0200

    feat: load kpis from dato

---
+++ src/client/store/data/kpiGroups.js
@@ -0,0 +1,20 @@
+import getData from '../../lib/get-data'
+
+export const state = () => []
+
+export const mutations = {
+  addKpiGroup(state, group) {
+    const kpis = group.kpis.map(kpi => ({
+      ...kpi,
+      unit: kpi.unit.key,
+    }))
+    state.push({ ...group, kpis })
+  },
+}
+
+export const actions = {
+  async getKpiGroups({ commit, dispatch }, locale) {
+    const data = await getData({ locale, slug: 'kpi-groups' })
+    data.kpiGroups.forEach(setting => commit('addKpiGroup', setting))
+  },
+}
