2c66e61d09278096d82be7a503f968cbb48d4700
Peter Goes <petergoes@gmail.com>
2018-10-05 10:00:20 +0200

    refactor(index): add file input

--- src/client/pages/index.vue
+++ src/client/pages/index.vue
@@ -11,27 +11,43 @@
         {{ $t('start_new_project') }}
       </md-button>
       <div class="page-index__divider" />
-      <md-button
-        :disabled="!acceptedLegal"
-        class="md-raised md-primary">
-        {{ $t('import_exisiting_project') }}
-      </md-button>
+      <div class="page-index__import-button-wrapper">
+        <md-button
+          :disabled="!acceptedLegal"
+          class="md-raised md-primary">
+          {{ $t('import_exisiting_project') }}
+        </md-button>
+        <input
+          class="page-index__input-file"
+          type="file"
+          accept="application/json"
+          @change="onFileInput">
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import { mapState, mapGetters } from "vuex";
+import { mapState, mapGetters, mapActions } from "vuex";
 import MapEventBus, { REDRAW } from "../lib/map-event-bus"
 
 export default {
   layout: 'modal',
   computed: {
     ...mapState('i18n', ['locale']),
-    ...mapGetters('flow', ['acceptedLegal']),
+    ...mapGetters('flow', ['acceptedLegal', 'currentFilledInLevel']),
   },
   mounted() {
     MapEventBus.$emit(REDRAW)
+  },
+  methods: {
+    ...mapActions({
+      importProject: 'project/importProject',
+    }),
+    async onFileInput(event) {
+      this.importProject(event)
+        .then(() => this.$router.push(this.currentFilledInLevel.uri))
+    },
   },
 }
 </script>
@@ -51,6 +67,25 @@ export default {
   height: 50px;
   background-color: lightgrey;
   flex: 1;
+}
+
+.page-index__import-button-wrapper {
+  display: flex;
+  position: relative;
+  z-index: 0;
+}
+
+.page-index__import-button-wrapper > * {
+  flex: 1;
+  z-index: 0;
+}
+
+.page-index__input-file {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  z-index: 1;
+  opacity: 0;
 }
 
 @supports (display: grid) {
