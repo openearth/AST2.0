f0efaf1f3209bd44b4ba3a7d59129e8d521eb242
bruna <brunadafonseca@gmail.com>
2018-10-11 09:29:41 +0200

    feat: add page

---
+++ src/client/pages/documentation.vue
@@ -0,0 +1,44 @@
+<template>
+  <md-drawer md-permanent="clipped" class="documentation">
+    <button class="md-link" @click="$router.go(-1)">&#x2190; {{ $t('back') }}</button>
+    <div class="documentation__content">
+      <h1 class="md-title">{{ documentation.title }}</h1>
+      <rich-text :text="documentation.content" />
+    </div>
+  </md-drawer>
+</template>
+
+<script>
+import { RichText, ResponsiveImage } from '~/components'
+import getData from '~/lib/get-data'
+
+export default {
+  layout: 'inactive-map',
+  components: { RichText, ResponsiveImage },
+  async asyncData({ params, store }) {
+    const { locale } = store.state.i18n
+    const data = await getData({ locale, slug: 'documentation' })
+    return { documentation: { ...data.legal } }
+  },
+}
+</script>
+
+<style>
+.legal {
+  width: var(--width-large);
+  padding: var(--spacing-double);
+}
+
+.legal p {
+  margin-bottom: var(--spacing-default);
+}
+
+.legal .md-link {
+  margin: 0;
+  padding: 0;
+  color: var(--action-color);
+  display: block;
+  margin-left: auto;
+  margin-bottom: var(--spacing-double);
+}
+</style>
