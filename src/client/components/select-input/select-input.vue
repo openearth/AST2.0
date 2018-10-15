71f3e9bcda52464de9ee52948fb9a731e724f14c
bruna <brunadafonseca@gmail.com>
2018-10-15 14:13:08 +0200

    feat: create new component

---
+++ src/client/components/select-input/select-input.vue
@@ -0,0 +1,43 @@
+<template>
+  <md-field>
+    <md-select v-model="value" md-dense>
+      <md-option
+        v-for="option in options"
+        :key="option.value"
+        :value="option.value"
+      >
+        {{ option.value }}
+      </md-option>
+    </md-select>
+  </md-field>
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  props: {
+    selectedValue: {
+      type: String,
+      required: true,
+    },
+    options: {
+      type: Array,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      value: this.searchValue,
+    }
+  },
+  watch: {
+    value(val) {
+      console.log(val)
+      this.$emit('onSelect', val)
+    },
+  },
+}
+</script>
+
