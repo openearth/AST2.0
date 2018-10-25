cbe5799729ed4ff2b3e1ada76ad4feb80dcc565b
Peter Goes <petergoes@gmail.com>
2018-10-25 11:24:14 +0200

    refactor: tweak legend styling

--- src/client/components/layer-legend/layer-legend.vue
+++ src/client/components/layer-legend/layer-legend.vue
@@ -1,26 +1,34 @@
 <template>
-  <div class="layer-legend">
-    <p class="layer-legend__title"> legend </p>
-    <md-list class="layer-legend__list">
-      <li
-        v-for="layer in layers"
-        :key="layer.id"
-        class="md-list-item">
-        <div
-          v-if="layer.showLegend"
-          class="md-list-item-content">
-          <p>{{ layer.title }}</p>
-          <img
-            :src="layer.legendUrl"
-            alt="">
-        </div>
-      </li>
-    </md-list>
-  </div>
+  <md-list class="layer-legend">
+    <md-list-item
+      :md-expanded="shownLayers"
+      :md-expand="shownLayers"
+      class="layer-legend__wrapper">
+      <span>{{ $t('legend') }}</span>
+
+      <div
+        v-if="shownLayers"
+        slot="md-expand"
+        class="layer-legend__content">
+        <md-list>
+          <md-list-item
+            v-for="layer in layers"
+            v-if="layer.showLegend"
+            :key="layer.id">
+            <div class="layer-legend__item">
+              <p>{{ layer.title }}</p>
+              <img
+                :src="layer.legendUrl"
+                alt="">
+            </div>
+          </md-list-item>
+        </md-list>
+      </div>
+    </md-list-item>
+  </md-list>
 </template>
 
 <script>
-
 export default {
   props: {
     layers: {
@@ -28,35 +36,37 @@ export default {
       default: () => [],
     },
   },
+  computed: {
+    shownLayers() {
+      return !!this.layers
+        .filter(layer => layer.showLegend)
+        .length
+    },
+  },
 }
 </script>
 
 <style>
-  .layer-legend
-  {
-    width:164px;
-    margin:6px;
-    background-color: #FFFFFF;
-    max-height: calc( 100% - 12px );
-    overflow-y:auto;
-    box-shadow: 0 3px 1px -2px rgba(0,0,0,.2), 0 2px 2px 0 rgba(0,0,0,.14), 0 1px 5px 0 rgba(0,0,0,.12);
-   }
+.layer-legend {
+  margin:6px;
+  padding: 0;
+  background-color: #FFFFFF;
+  max-height: calc(100vh - 12px - 70px);
+  overflow: hidden;
+  box-shadow: 0 3px 1px -2px rgba(0,0,0,.2), 0 2px 2px 0 rgba(0,0,0,.14), 0 1px 5px 0 rgba(0,0,0,.12);
+}
 
-  .layer-legend__title
-  {
-    padding:4px;
-    background-color: #D4D4D4;
-    height:32px;
-  }
+.layer-legend__wrapper {
+  width: 200px;
+}
 
-  .layer-legend__list
-  {
-    padding: 0;
-  }
+.layer-legend__content {
+  overflow-y: auto;
+  max-height: calc(100vh - 12px - 120px);
+}
 
-  .layer-legend__list .md-list-item-content
-  {
-    display:block;
-    font-size:smaller;
-  }
+.layer-legend__item {
+  display: flex;
+  flex-direction: column;
+}
 </style>
