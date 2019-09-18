737e20d7d194deb2b352e49c9b73d3015bbb96b5
CindyvdVries <cindyvdvries@gmail.com>
2019-09-18 11:04:39 +0200

    create layer dialog

---
+++ src/client/components/layer-dialog/layer-dialog.vue
@@ -0,0 +1,19 @@
+<template>
+  <md-dialog layout-padding>
+    <h2>Enter link to background layer</h2>
+    <p>
+      - enter link to server
+      - enter link type
+    </p>
+  </md-dialog>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
