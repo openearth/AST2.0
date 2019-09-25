776548a3fbf343a227236c552270b709bac24f47
Benedikt Deitmer <you@hornebom.com>
2019-09-25 15:36:35 +0200

    create BackButton comp and add it to About, Docu and Legal pages

---
+++ src/client/components/back-button/back-button.vue
@@ -0,0 +1,14 @@
+<template>
+  <header class="back-button__container">
+    <md-button class="md-raised" @click="$router.go(-1)">
+      <md-icon>keyboard_arrow_left</md-icon>
+      {{ $t('back') }}
+    </md-button>
+  </header>
+</template>
+
+<style>
+.back-button__container {
+  text-align: right;
+}
+</style>
