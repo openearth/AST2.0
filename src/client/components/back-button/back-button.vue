fa192db4588416b195b71a4af9188a806807d80e
Benedikt Deitmer <you@hornebom.com>
2019-10-01 10:32:43 +0200

    PR feedback (router back method, type of button, additional instance of component in measure detail)

--- src/client/components/back-button/back-button.vue
+++ src/client/components/back-button/back-button.vue
@@ -1,14 +1,35 @@
 <template>
   <header class="back-button__container">
-    <md-button class="md-raised" @click="$router.go(-1)">
-      <md-icon>keyboard_arrow_left</md-icon>
-      {{ $t('back') }}
-    </md-button>
+    <section class="back-button__inner">
+      <md-button class="back-button__btn" @click="$router.back()">
+        <md-icon>keyboard_arrow_left</md-icon>
+        {{ $t('back') }}
+      </md-button>
+    </section>
+    <section class="back-button__slot">
+      <slot />
+    </section>
   </header>
 </template>
 
 <style>
 .back-button__container {
+  display: flex;
+  justify-content: space-between;
+}
+
+.back-button__inner {
+  margin-right: 15px;
+  text-align: left;
+}
+
+.back-button__btn {
+  position: relative;
+  left: -23px;
+}
+
+.back-button__slot {
   text-align: right;
 }
+
 </style>
