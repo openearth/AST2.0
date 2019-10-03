a6f9d077a94c380c0d67a6875d7f8c98e2c933d7
CindyvdVries <cindyvdvries@gmail.com>
2019-10-03 16:21:37 +0200

    Retrieve maplayers from Dato

--- src/client/middleware/core-data.js
+++ src/client/middleware/core-data.js
@@ -34,6 +34,10 @@ export default async function ({ store }) {
     store.dispatch('data/wmsLayers/getWmsLayers', store.state.i18n.locale)
   }
 
+  if (!store.state.data.mapLayers.length) {
+    store.dispatch('data/mapLayers/getMapLayers', store.state.i18n.locale)
+  }
+
   if (!Object.keys(store.state.data.workspaces).filter(ItemsWithoutUnderscore).length) {
     store.dispatch('data/workspaces/getWorkspaces')
   }
---
+++ src/client/store/data/mapLayers.js
@@ -0,0 +1,39 @@
+import getData from '../../lib/get-data'
+import randomColor from '../../lib/random-color';
+import { getRealApiData } from "../../lib/get-api-data";
+export const state = () => []
+
+export const mutations = {
+  addLayer(state, layer) {
+    state.push(layer)
+  },
+}
+
+export const actions = {
+  async getMapLayers({ commit, dispatch }, locale) {
+    const data = await getData({ locale, slug: 'map-layers' })
+    data.mapLayers.forEach(layer => {
+      commit('addLayer', layer)
+    })
+    dispatch('project/bootstrapMapLayers', data.mapLayers, { root: true })
+  },
+}
+
+export const getters = {
+  constructed(state) {
+    console.log('hoi')
+    return state.map(layer => {
+      return {
+       id: layer.id,
+       title: layer.title,
+       layerType: 'raster',
+       url: layer.url,
+       showLegend: false,
+       tilesize: 256,
+       opacity: 1,
+       visible: true,
+       backgroundColor: randomColor(),
+     }
+    })
+  },
+}
