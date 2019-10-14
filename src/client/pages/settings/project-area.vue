edff42ccb3e052fd13f00c71bdbc34cdf848c264
Benedikt Deitmer <you@hornebom.com>
2019-10-14 14:35:27 +0200

    remove outer list (expandable) ++ update styling for info button

--- src/client/pages/settings/project-area.vue
+++ src/client/pages/settings/project-area.vue
@@ -7,85 +7,81 @@
       <md-button :to="`/${locale}/new-project`" class="md-primary">{{ $t('change_area') }}</md-button>
     </md-toolbar>
 
-    <form>
-      <md-list>
-        <md-list-item
-          v-for="setting in areaSettings"
-          :key="setting.key"
-          :md-expand="true"
-          :md-expanded="true">
-          <span class="md-list-item-text">{{ setting.title }}</span>
+    <form class="project-area__form">
+      <section
+        v-for="setting in areaSettings"
+        :key="setting.key"
+        class="project-area__group"
+      >
+        <span class="md-list-item-text">{{ setting.title }}</span>
 
-          <md-list
-            v-if="!setting.isSelect"
-            slot="md-expand"
-            class="project-area__options">
-            <md-list-item
-              v-for="option in setting.options"
-              :key="option.value">
-              <md-checkbox
-                v-if="setting.multiple && !setting.isSelect"
-                :value="!projectAreaSettings[setting.key][option.value]"
+        <md-list
+          v-if="!setting.isSelect"
+          class="project-area__options">
+          <md-list-item
+            v-for="option in setting.options"
+            :key="option.value">
+            <md-checkbox
+              v-if="setting.multiple && !setting.isSelect"
+              :value="!projectAreaSettings[setting.key][option.value]"
+              @change="value => updateProjectAreaSetting({
+                type: 'checkbox',
+                key: setting.key,
+                option: option.value,
+                value
+              })"
+            />
+            <md-radio
+              v-else
+              :value="projectAreaSettings[setting.key] !== option.value"
+              required
+              @change="value => updateProjectAreaSetting({
+                type: 'radio',
+                key: setting.key,
+                value: option.value,
+              })"
+            />
+            <span class="md-list-item-text">{{ option.title }}</span>
+          </md-list-item>
+          <md-button
+            v-if="setting.infoText"
+            class="md-icon-button info-button"
+            @click="setActiveTooltip(setting.key)">
+            <md-icon>info</md-icon>
+            <md-tooltip :md-active="activeTooltipKey === setting.key" md-direction="top">
+              {{ setting.infoText }}
+            </md-tooltip>
+          </md-button>
+        </md-list>
+
+        <md-list
+          v-else
+          class="project-area__select">
+          <div style="max-width: 90%; flex-grow: 1;">
+            <md-list-item>
+              <select-input
+                :options="setting.options"
+                :value="projectAreaSettings[setting.key]"
+                :id="setting.key"
                 @change="value => updateProjectAreaSetting({
-                  type: 'checkbox',
+                  type: 'select',
                   key: setting.key,
-                  option: option.value,
-                  value
+                  value,
                 })"
               />
-              <md-radio
-                v-else
-                :value="projectAreaSettings[setting.key] !== option.value"
-                required
-                @change="value => updateProjectAreaSetting({
-                  type: 'radio',
-                  key: setting.key,
-                  value: option.value,
-                })"
-              />
-              <span class="md-list-item-text">{{ option.title }}</span>
             </md-list-item>
-            <md-button
-              v-if="setting.infoText"
-              class="md-icon-button info-button"
-              @click="setActiveTooltip(setting.key)">
-              <md-icon>info</md-icon>
-              <md-tooltip :md-active="activeTooltipKey === setting.key" md-direction="top">
-                {{ setting.infoText }}
-              </md-tooltip>
-            </md-button>
-          </md-list>
-
-          <md-list
-            v-else
-            slot="md-expand"
-            class="project-area__select">
-            <div style="max-width: 90%; flex-grow: 1;">
-              <md-list-item>
-                <select-input
-                  :options="setting.options"
-                  :value="projectAreaSettings[setting.key]"
-                  :id="setting.key"
-                  @change="value => updateProjectAreaSetting({
-                    type: 'select',
-                    key: setting.key,
-                    value,
-                  })"
-                />
-              </md-list-item>
-            </div>
-            <md-button
-              v-if="setting.infoText"
-              class="md-icon-button"
-              @click="setActiveTooltip(setting.key)">
-              <md-icon>info</md-icon>
-              <md-tooltip :md-active="activeTooltipKey === setting.key" md-direction="top">
-                {{ setting.infoText }}
-              </md-tooltip>
-            </md-button>
-          </md-list>
-        </md-list-item>
-      </md-list>
+          </div>
+          <md-button
+            v-if="setting.infoText"
+            class="md-icon-button"
+            @click="setActiveTooltip(setting.key)">
+            <md-icon>info</md-icon>
+            <md-tooltip :md-active="activeTooltipKey === setting.key" md-direction="top">
+              {{ setting.infoText }}
+            </md-tooltip>
+          </md-button>
+        </md-list>
+      </section>
     </form>
   </div>
 </template>
@@ -137,8 +133,17 @@ export default {
   justify-content: space-between;
 }
 
-.project-area .md-list-item-expand {
-  border: none;
+.project-area__form {
+  margin-top: var(--spacing-large);
+}
+
+.project-area__group {
+  margin-top: var(--spacing-medium);
+}
+
+.project-area__group > .md-list-item-text {
+  padding-right: calc( var(--spacing-default) * 2 ); /* space for info button */
+  padding-left: var(--spacing-default);
 }
 
 .project-area__select {
@@ -155,7 +160,7 @@ export default {
 
 .info-button {
   position: absolute;
-  top: 5px;
+  top: -32px;
   right: 2px;
 }
 </style>
