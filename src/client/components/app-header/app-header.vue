6ebad3ab0b7f65c13ef027b6fdb441f786b55afb
Peter Goes <petergoes@gmail.com>
2019-11-05 16:03:59 +0100

    Prevent mode change when clicking on learn more in set-measure
    The map should not redraw itself. Causes major performance issues

--- src/client/components/app-header/app-header.vue
+++ src/client/components/app-header/app-header.vue
@@ -1,7 +1,11 @@
 <template>
   <md-toolbar class="md-primary" role="banner">
     <section class="md-toolbar-section-start">
-      <md-button class="md-icon-button" @click="() => $emit('onShowNavigation')">
+      <md-button
+        :disabled="inSetMeasureFlow"
+        :class="{'app-header__button--disabled': inSetMeasureFlow}"
+        class="md-icon-button"
+        @click="() => $emit('onShowNavigation')">
         <md-icon>menu</md-icon>
       </md-button>
 
@@ -27,20 +31,25 @@
     <section class="md-toolbar-section-end">
       <md-button
         v-if="currentFilledInLevel.level >= LEVEL_PROJECT_AREA"
+        :disabled="inSetMeasureFlow"
+        :class="{'app-header__button--disabled': inSetMeasureFlow}"
         :key="2"
         :to="`/${this.$i18n.locale}/settings/project-area/`"
         class="md-primary md-icon-button">
         <md-icon>settings</md-icon>
       </md-button>
       <fullscreen-button />
-      <LogIn v-if="legalAccepted" />
+      <LogIn
+        v-if="legalAccepted"
+        :disabled="inSetMeasureFlow"
+        :class="{'app-header__button--disabled': inSetMeasureFlow}"/>
     </section>
   </md-toolbar>
 </template>
 
 <script>
 import startCase from 'lodash/startCase'
-import { mapGetters } from "vuex";
+import { mapGetters, mapState } from "vuex";
 import { LEVEL_PROJECT_AREA } from "../../lib/flow-levels";
 import FullscreenButton from '../fullscreen-button'
 import LogIn from '../log-in'
@@ -67,6 +76,7 @@ export default {
   data: () => ({ LEVEL_PROJECT_AREA }),
   computed: {
     ...mapGetters('flow', ['currentFilledInLevel']),
+    ...mapState({ inSetMeasureFlow: state => state.setMeasureFlow.inFlow }),
     hasTitle() {
       return !!this.projectTitle
     },
@@ -76,3 +86,9 @@ export default {
   },
 }
 </script>
+
+<style>
+.app-header__button--disabled {
+  opacity: 0.5;
+}
+</style>
