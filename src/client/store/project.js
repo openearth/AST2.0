8724c4e897373808c96e03315d435805ec81f65a
CindyvdVries <cindyvdvries@gmail.com>
2020-08-14 07:20:53 +0200

    filter out hidden areas for heatstress calculation

--- src/client/store/project.js
+++ src/client/store/project.js
@@ -373,6 +373,9 @@ export const actions = {
   fetchHeatstressData({ state, commit, getters, dispatch }) {
     commit('clearHeatstressLayers')
     let features = cloneDeep(getters.areas)
+
+    // Only use the areas that are not hidden
+    features.filter(feat => !feat.properties.hidden)
     // If the properties are not set use the defaults
     features.forEach(area => {
       area.properties.areaInflow = area.properties.areaInflow ||  area.properties.defaultInflow
