dbf7d91b3183d2388f3591bebd7773e8dc3244b3
Bob Bol <bob@voorhoede.nl>
2020-08-18 16:12:13 +0200

    refactor to single card for single and multi selection

--- src/client/pages/project/areas.vue
+++ src/client/pages/project/areas.vue
@@ -4,139 +4,136 @@
       <span class="md-title">{{ $t('selected_measures') }}</span>
     </md-toolbar>
 
-    <ul class="areas__list">
-      <li
-        v-for="({ feature, measure }) in featuresWithMeasure"
-        :key="feature.id"
-        class="areas__item"
+    <md-card v-if="hasSelection">
+      <div
+        :style="`border-left-color: ${ combinedMeasure ? combinedMeasure.color.hex : 'transparent' }`"
+        class="areas__item-content"
       >
-        <md-card>
-          <div
-            :style="`border-left-color: ${ measure ? measure.color.hex : 'transparent' }`"
-            class="areas__item-content"
-          >
-            <md-card-header>
-              <md-avatar class="areas__avatar">
-                <img
-                  v-if="measure"
-                  :src="measure.image.url"
-                  alt="Avatar"
-                >
-              </md-avatar>
+        <md-card-header>
+          <md-avatar class="areas__avatar">
+            <img
+              v-if="combinedMeasure"
+              :src="combinedMeasure.image.url"
+              alt="Avatar"
+            >
+          </md-avatar>
 
-              <div class="md-title">
-                {{ feature.properties.name }}
-              </div>
-              <div class="md-subhead areas__subhead">
-                {{ measure ? measure.title : ' ' }}
-              </div>
-            </md-card-header>
-
-            <md-card-content class="areass__card-content">
-              <text-input
-                :label="$t('area_name')"
-                :value="feature.properties.name"
-                :on-change="name => updateAreaProperty({ id: feature.id, properties: { name }})"
-              />
-
-              <span class="md-subheading">{{ $t('measure') }}</span>
-              <div class="areas__choose-wrapper">
-                <div class="areas__choose-content">
-                  <p v-if="measure">
-                    {{ measure.title }}
-                  </p>
-                  <md-button
-                    :to="`/${locale}/project/measures`"
-                    class="md-accent md-raised areas__choose-button"
-                  >
-                    {{ measure ? $t('change_measure') : $t('choose_measure') }}
-                  </md-button>
-                </div>
-                <div v-if="measure" class="areas__choose-icon">
-                  <img :src="measure.image.url">
-                </div>
-              </div>
-
-              <!-- Depth input -->
-              <input-range
-                v-if="measure && getDefaultValueProperty('depth', 'show', measure.defaultValues)"
-                :value="inputValue(feature.properties.areaDepth, feature.properties.defaultDepth)"
-                :min="getDefaultValueProperty('depth', 'min', measure.defaultValues)"
-                :max="getDefaultValueProperty('depth', 'max', measure.defaultValues)"
-                :label="$t('area_depth')"
-                @change="value => updateAreaProperties({ features: [feature], properties: { areaDepth: value }})"
-              >
-                <template v-slot:info>
-                  <app-tooltip
-                    :message="$t('area_depth_info')"
-                    class="areas__info-button"
-                    direction="left"
-                  />
-                </template>
-              </input-range>
-
-              <!-- Inflow input -->
-              <input-range
-                v-if="measure && getDefaultValueProperty('inflow', 'show', measure.defaultValues)"
-                :value="inputValue(feature.properties.areaInflow, feature.properties.defaultInflow)"
-                :min="getDefaultValueProperty('inflow', 'min', measure.defaultValues)"
-                :max="getDefaultValueProperty('inflow', 'max', measure.defaultValues)"
-                :label="$t('area_inflow')"
-                @change="value => updateAreaProperties({ features: [feature], properties: { areaInflow: value }})"
-              >
-                <template v-slot:info>
-                  <app-tooltip
-                    :message="$t('area_inflow_info')"
-                    class="areas__info-button"
-                    direction="left"
-                  />
-                </template>
-              </input-range>
-
-              <!-- Width input -->
-              <input-range
-                v-if="measure && feature.geometry.type === 'LineString' && getDefaultValueProperty('width', 'show', measure.defaultValues)"
-                :value="inputValue(feature.properties.areaWidth, feature.properties.defaultWidth)"
-                :min="getDefaultValueProperty('width', 'min', measure.defaultValues)"
-                :max="getDefaultValueProperty('width', 'max', measure.defaultValues)"
-                :label="$t('area_width')"
-                @change="value => updateAreaProperties({ features: [feature], properties: { areaWidth: value }})"
-              >
-                <template v-slot:info>
-                  <app-tooltip
-                    :message="$t('area_width_info')"
-                    class="areas__info-button"
-                  />
-                </template>
-              </input-range>
-
-              <!-- Radius input -->
-              <input-range
-                v-if="measure && feature.geometry.type === 'Point' && getDefaultValueProperty('radius', 'show', measure.defaultValues)"
-                :value="inputValue(feature.properties.areaRadius, feature.properties.defaultRadius)"
-                :min="getDefaultValueProperty('radius', 'min', measure.defaultValues)"
-                :max="getDefaultValueProperty('radius', 'max', measure.defaultValues)"
-                :label="$t('area_radius')"
-                @change="value => updateAreaProperties({ features: [feature], properties: { areaRadius: value }})"
-              >
-                <template v-slot:info>
-                  <app-tooltip
-                    :message="$t('area_radius_info')"
-                    class="areas__info-button"
-                  />
-                </template>
-              </input-range>
-            </md-card-content>
-
-            <md-card-actions>
-              <md-button @click="onDone">
-                {{ $t('done') }}
-              </md-button>
-            </md-card-actions>
+          <div class="md-title">
+            {{ combinedFeature.properties.name }}
           </div>
-        </md-card>
-      </li>
-    </ul>
+          <div class="md-subhead areas__subhead">
+            {{ combinedMeasure ? combinedMeasure.title : ' ' }}
+          </div>
+        </md-card-header>
+
+        <md-card-content>
+          <!-- Name input -->
+          <text-input
+            v-if="isSingleSelection"
+            :label="$t('area_name')"
+            :value="combinedFeature.properties.name"
+            :on-change="name => updateAreaProperty({ id: feature.id, properties: { name }})"
+          />
+
+          <!-- Measure type input -->
+          <span class="md-subheading">
+            {{ $t('measure') }}
+          </span>
+          <div class="areas__choose-wrapper">
+            <div class="areas__choose-content">
+              <p v-if="combinedMeasure">
+                {{ combinedMeasure.title }}
+              </p>
+              <md-button
+                :to="`/${locale}/project/measures`"
+                class="md-accent md-raised areas__choose-button"
+              >
+                {{ combinedMeasure ? $t('change_measure') : $t('choose_measure') }}
+              </md-button>
+            </div>
+            <div v-if="combinedMeasure" class="areas__choose-icon">
+              <img :src="combinedMeasure.image.url">
+            </div>
+          </div>
+
+          <!-- Depth input -->
+          <!-- <input-range
+            v-if="combinedMeasure && getDefaultValueProperty('depth', 'show', combinedMeasure.defaultValues)"
+            :value="inputValue(combinedFeature.properties.areaDepth, combinedFeature.properties.defaultDepth)"
+            :min="getDefaultValueProperty('depth', 'min', combinedMeasure.defaultValues)"
+            :max="getDefaultValueProperty('depth', 'max', combinedMeasure.defaultValues)"
+            :label="$t('area_depth')"
+            @change="updateValue('areaDepth', combinedFeature, $event)"
+          >
+            <template v-slot:info>
+              <app-tooltip
+                :message="$t('area_depth_info')"
+                class="areas__info-button"
+                direction="left"
+              />
+            </template>
+          </input-range> -->
+
+          <!-- Inflow input -->
+          <!-- <input-range
+            v-if="combinedMeasure && getDefaultValueProperty('inflow', 'show', combinedMeasure.defaultValues)"
+            :value="inputValue(combinedFeature.properties.areaInflow, combinedFeature.properties.defaultInflow)"
+            :min="getDefaultValueProperty('inflow', 'min', combinedMeasure.defaultValues)"
+            :max="getDefaultValueProperty('inflow', 'max', combinedMeasure.defaultValues)"
+            :label="$t('area_inflow')"
+            @change="value => updateAreaProperties({ features: [combinedFeature], properties: { areaInflow: value }})"
+          >
+            <template v-slot:info>
+              <app-tooltip
+                :message="$t('area_inflow_info')"
+                class="areas__info-button"
+                direction="left"
+              />
+            </template>
+          </input-range> -->
+
+          <!-- Width input -->
+          <!-- <input-range
+            v-if="combinedMeasure && combinedFeature.geometry.type === 'LineString' && getDefaultValueProperty('width', 'show', combinedMeasure.defaultValues)"
+            :value="inputValue(combinedFeature.properties.areaWidth, combinedFeature.properties.defaultWidth)"
+            :min="getDefaultValueProperty('width', 'min', combinedMeasure.defaultValues)"
+            :max="getDefaultValueProperty('width', 'max', combinedMeasure.defaultValues)"
+            :label="$t('area_width')"
+            @change="value => updateAreaProperties({ features: [combinedFeature], properties: { areaWidth: value }})"
+          >
+            <template v-slot:info>
+              <app-tooltip
+                :message="$t('area_width_info')"
+                class="areas__info-button"
+              />
+            </template>
+          </input-range> -->
+
+          <!-- Radius input -->
+          <!-- <input-range
+            v-if="combinedMeasure && combinedFeature.geometry.type === 'Point' && getDefaultValueProperty('radius', 'show', combinedMeasure.defaultValues)"
+            :value="inputValue(combinedFeature.properties.areaRadius, combinedFeature.properties.defaultRadius)"
+            :min="getDefaultValueProperty('radius', 'min', combinedMeasure.defaultValues)"
+            :max="getDefaultValueProperty('radius', 'max', combinedMeasure.defaultValues)"
+            :label="$t('area_radius')"
+            @change="value => updateAreaProperties({ features: [combinedFeature], properties: { areaRadius: value }})"
+          >
+            <template v-slot:info>
+              <app-tooltip
+                :message="$t('area_radius_info')"
+                class="areas__info-button"
+              />
+            </template>
+          </input-range> -->
+        </md-card-content>
+
+        <md-card-actions>
+          <md-button @click="onDone">
+            {{ $t('done') }}
+          </md-button>
+        </md-card-actions>
+      </div>
+    </md-card>
   </aside>
 </template>
 
@@ -167,6 +164,40 @@ export default {
         measure: this.measureById(feature.properties.measure),
       }))
     },
+
+    hasSelection() {
+      return this.selectedFeatures.length > 0
+    },
+
+    isSingleSelection() {
+      return this.selectedFeatures.length === 1
+    },
+
+    isMultiSelection() {
+      return this.selectedFeatures.length > 1
+    },
+
+    isEditableMultiSelection() {
+      return true
+    },
+
+    combinedFeature() {
+      if(this.isSingleSelection) {
+        return this.selectedFeatures[0]
+      }
+      else {
+        return null
+      }
+    },
+
+    combinedMeasure() {
+      if(this.isSingleSelection) {
+        return this.measureById(this.selectedFeatures[0].properties.measure)
+      }
+      else {
+        return null
+      }
+    },
   },
   mounted() {
     MapEventBus.$emit(REDRAW)
@@ -195,6 +226,14 @@ export default {
       let returnValue = value || defaultValue
       returnValue = value === '' ? value : returnValue
       return returnValue
+    },
+    updateValue(setting, feature, value) {
+      this.updateAreaProperties({
+        features: [ feature ],
+        properties: {
+          [setting]: value,
+        },
+      })
     },
   },
 }
