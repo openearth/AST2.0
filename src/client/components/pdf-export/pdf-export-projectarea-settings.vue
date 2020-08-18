edc7956c781d5765ba1e244ce8d7f4da751a975c
Peter Goes <petergoes@gmail.com>
2020-08-18 17:07:15 +0200

    Add pdf improvements
    * Logos on first page
    * Deltares logo as footer
    * intro has line breaks
    * Headings correct color
    * add wordbreaking
    * Add targets

--- src/client/components/pdf-export/pdf-export-projectarea-settings.vue
+++ src/client/components/pdf-export/pdf-export-projectarea-settings.vue
@@ -3,20 +3,48 @@
     <h2 class="md-title">
       {{ $t('pdf_projectarea_characteristics') }}
     </h2>
-    <p class="pdf-export-projectarea-settings__intro">
-      {{ $t('pdf_used_projectarea_settings') }}
-    </p>
 
-    <dl class="pdf-export-projectarea-settings__description-list">
-      <template v-for="setting in settings">
-        <dt :key="`${setting.term}-term`">
-          {{ setting.term }}:
-        </dt>
-        <dd :key="`${setting.description}-description`">
-          {{ setting.description }}
-        </dd>
-      </template>
-    </dl>
+    <section class="pdf-export-projectarea-settings__wrapper">
+      <article>
+        <p class="pdf-export-projectarea-settings__intro">
+          {{ $t('pdf_used_projectarea_settings') }}
+        </p>
+        <dl class="pdf-export-projectarea-settings__description-list">
+          <template v-for="setting in settings">
+            <dt :key="`${setting.term}-term`">
+              {{ setting.term }}:
+            </dt>
+            <dd :key="`${setting.description}-description`">
+              {{ setting.description }}
+            </dd>
+          </template>
+        </dl>
+      </article>
+      <article>
+        <p class="pdf-export-projectarea-settings__intro">
+          {{ $t('pdf_used_projectarea_targets') }}
+        </p>
+        <section
+          v-for="targetGroup in formattedTargets"
+          :key="targetGroup.title"
+          class="pdf-export-projectarea-settings__targets"
+        >
+          <header class="md-body-2">
+            {{ targetGroup.title }}
+          </header>
+          <dl class="pdf-export-projectarea-settings__description-list">
+            <template v-for="kpi in targetGroup.kpis">
+              <dt :key="`${kpi.key}-term`">
+                {{ kpi.key }}:
+              </dt>
+              <dd :key="`${kpi.value}-description`">
+                {{ kpi.value }}
+              </dd>
+            </template>
+          </dl>
+        </section>
+      </article>
+    </section>
   </article>
 </template>
 
@@ -27,6 +55,8 @@ export default {
     ...mapState({
       projectSettings: state => state.project.settings.projectArea,
       areaSettings: state => state.data.areaSettings,
+      kpiGroups: state => state.data.kpiGroups,
+      targets: state => state.project.settings.targets,
     }),
     ...mapGetters('flow', ['currentFilledInLevel']),
     ...mapGetters('data/workspaces', ['scenariosInActiveWorkspace']),
@@ -52,11 +82,36 @@ export default {
           return { term, description }
         })
     },
+    formattedTargets() {
+      const foo = this.kpiGroups
+        .map(group => {
+          return {
+            title: group.title,
+            kpis: Object.entries(this.targets[group.key])
+              .filter(([, value]) => value.include)
+              .map(([key, { value }]) => ({
+                key: group.kpis.find(kpi => kpi.key === key).title,
+                value,
+              })),
+          }
+        })
+        .filter(group => group.kpis.length)
+      console.log(foo)
+      return foo
+    },
   },
 }
 </script>
 
 <style>
+.pdf-export-projectarea-settings__wrapper {
+  display: flex;
+}
+
+.pdf-export-projectarea-settings__wrapper > * {
+  flex: 1;
+}
+
 .pdf-export-projectarea-settings__description-list {
   display: grid;
   row-gap: 0.5rem;
@@ -67,5 +122,9 @@ export default {
 .pdf-export-projectarea-settings__intro {
   padding-top: 1rem;
   padding-bottom: 1rem;
+}
+
+.pdf-export-projectarea-settings__targets:not(:first-of-type) {
+  margin-top: var(--spacing-default);
 }
 </style>
