0f9a55f556d41d226c10fd52ceee4fb0215edb8c
Peter Goes <petergoes@gmail.com>
2020-12-04 15:56:38 +0100

    remove log statement

--- src/client/lib/calculate-fmeas-area.js
+++ src/client/lib/calculate-fmeas-area.js
@@ -8,8 +8,6 @@ export default function calculateFmeasArea(A_tot, A_p, Frac_RA, FMeas_areas = []
 
   const result = (A_p * e(FMeas / A_p) + Frac_RA * (A_tot - A_p)) / (A_p + Frac_RA * (A_tot - A_p))
 
-  console.log({ FMeas_areas, result })
-
   return isNaN(result)
     ? 0
     : result
