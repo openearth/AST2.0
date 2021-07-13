280dd00c4e823ed0b02eb2e2016e24276f505589
Peter Goes <petergoes@gmail.com>
2021-07-13 13:08:55 +0200

    Update convertToMetric comments

--- src/client/components/unit-output/convert-to-metric.js
+++ src/client/components/unit-output/convert-to-metric.js
@@ -2,19 +2,19 @@ export default function convertToMetric(value, unitKey) {
   switch (unitKey) {
     case 'DistanceMM':
     case 'Ratemmy':
-      // https://www.metric-conversions.org/length/centimeters-to-inches.htm
+      // https://www.metric-conversions.org/length/inches-to-centimeters.htm
       return value / 0.39370
     case 'distance':
-      // https://www.metric-conversions.org/length/meters-to-feet.htm
+      // https://www.metric-conversions.org/length/feet-to-meters.htm
       return value / 3.2808
     case 'surface':
-      // https://www.metric-conversions.org/area/square-meters-to-square-feet.htm
+      // https://www.metric-conversions.org/area/square-feet-to-square-meters.htm
       return value / 10.764
     case 'volume':
-      // https://www.metric-conversions.org/volume/cubic-meters-to-cubic-feet.htm
+      // https://www.metric-conversions.org/volume/cubic-feet-to-cubic-meters.htm
       return value / 35.315
     case 'temperature':
-      // https://www.metric-conversions.org/temperature/celsius-to-fahrenheit.htm
+      // https://www.metric-conversions.org/temperature/fahrenheit-to-celsius.htm
       return (value - 32) / 1.8000
     default:
       return value
