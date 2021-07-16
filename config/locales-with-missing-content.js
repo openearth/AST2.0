86f2d4ba684a58ed55b48ea806c2fe73a2f5fe85
Peter Goes <petergoes@gmail.com>
2021-07-16 15:38:45 +0200

    Let all locales fall back to en

--- config/locales-with-missing-content.js
+++ config/locales-with-missing-content.js
@@ -1,3 +1,7 @@
 module.exports = [
   { sourceLocale: 'en_US', baseLocale: 'en' },
+  { sourceLocale: 'es', baseLocale: 'en' },
+  { sourceLocale: 'nl', baseLocale: 'en' },
+  { sourceLocale: 'ru', baseLocale: 'en' },
+  { sourceLocale: 'zh_CN', baseLocale: 'en' },
 ]
